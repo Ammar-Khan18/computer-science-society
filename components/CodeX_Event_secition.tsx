b3575d562a0d096b17c79d810c827a200fecb7f3
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-21 15:57:34 +0500

    removed incorrect spelled file

--- components/CodeX_Event_secition.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CodeX_Event_secition = () => {
-  return (
-    <div>CodeX_Event_secition</div>
-  )
-}
-
-export default CodeX_Event_secition
---
+++ components/CodeX_Event_section.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CodeX_Event_section = () => {
+  return (
+    <div>CodeX_Event_section</div>
+  )
+}
+
+export default CodeX_Event_section
