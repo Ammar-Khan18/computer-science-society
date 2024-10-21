8fe101614d5fd9cde97c2f4d8fa330db466d0be0
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-21 15:55:23 +0500

    modified the page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import AboutUs_section from "@/components/AboutUs_section";
+import CodeX_Event_secition from "@/components/CodeX_Event_secition";
 import Hero_section from "@/components/Hero_section";
+import ProBattle_Event_section from "@/components/ProBattle_Event_section";
+import Social_section from "@/components/Social_section";
 
 export default function Home() {
   return (
     <>
       <Hero_section />
+      <CodeX_Event_secition />
+      <ProBattle_Event_section />
+      <Social_section />
+      <AboutUs_section />
     </>
   );
 }
