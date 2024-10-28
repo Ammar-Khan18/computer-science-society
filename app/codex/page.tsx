470f3851eb4556d18b35f6a2f19dd8f84794beb0
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-28 08:03:22 +0500

    added seperate hero section

--- app/codex/page.tsx
+++ app/codex/page.tsx
@@ -4,14 +4,13 @@ import WeeklyContests from '@/components/WeeklyContest';
 import MonthlyHackathons from '@/components/MonthlyHackathons';
 import ArenaResearchProgram from '@/components/ArenaResearchProgram';
 import Blogs from '@/components/codexBlogs';
+import CodeX_Hero_section from '@/components/CodeX_Hero_section';
 
 const CodeXPage = () => {
   return (
     <Box sx={{ minHeight: '100vh', backgroundColor: '#fdd835',color:'black' }}>
       {/* Title Section */}
-      <Typography variant="h2" sx={{ textAlign: 'center', padding: '2rem 0' }}>
-        CodeX Event
-      </Typography>
+      <CodeX_Hero_section />
 
       {/* Weekly Contests Section */}
       <WeeklyContests />
