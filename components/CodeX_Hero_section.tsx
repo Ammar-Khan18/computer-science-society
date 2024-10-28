0f5d20179b8997ce99eeee5b3a8f79c60fa1c1de
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-28 08:04:43 +0500

    made hero component

---
+++ components/CodeX_Hero_section.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from '@mui/material';
+
+const CodeX_Hero_section = () => {
+  return (
+    <Box sx={{ padding: '4rem 2rem', backgroundColor: '#f0f0f0' }}>
+        <Typography variant="h2" sx={{ textAlign: 'center', padding: '2rem 0' }}>
+            CodeX Event
+        </Typography>
+    </Box>
+  );
+};
+
+export default CodeX_Hero_section
