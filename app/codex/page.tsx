1f21752aaad2cfa3e4a250b0de4d7db14e466801
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-28 08:25:08 +0500

    added partners section

--- app/codex/page.tsx
+++ app/codex/page.tsx
@@ -5,11 +5,12 @@ import MonthlyHackathons from '@/components/MonthlyHackathons';
 import ArenaResearchProgram from '@/components/ArenaResearchProgram';
 import Blogs from '@/components/codexBlogs';
 import CodeX_Hero_section from '@/components/CodeX_Hero_section';
+import CodeX_Partners_section from '@/components/CodeX_Partners_section';
 
 const CodeXPage = () => {
   return (
     <Box sx={{ minHeight: '100vh', backgroundColor: '#fdd835',color:'black' }}>
-      {/* Title Section */}
+      {/* Hero Section */}
       <CodeX_Hero_section />
 
       {/* Weekly Contests Section */}
@@ -17,6 +18,9 @@ const CodeXPage = () => {
 
       {/* Monthly Hackathons Section */}
       <MonthlyHackathons />
+
+      {/* Partners Section */}
+      <CodeX_Partners_section />
 
       {/* Arena Research Program Section */}
       <ArenaResearchProgram />
