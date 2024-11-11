803344b99a3a5e6bb7bd238e6855f4ac6f579c65
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-11-11 10:03:48 +0500

    Update CodeX_ArenaResearchProgram.tsx

--- components/CodeX_ArenaResearchProgram.tsx
+++ components/CodeX_ArenaResearchProgram.tsx
@@ -1,21 +1,3 @@
-// import { Box, Container, Typography } from '@mui/material';
-
-// const ArenaResearchProgram = () => {
-//   return (
-//     <Container maxWidth="lg">
-//     <Box sx={{ padding: '4rem 2rem', backgroundColor: '#d0d0d0' }}>
-//       <Typography variant="h4" sx={{ mb: 2 }}>
-//         Arena Research Program
-//       </Typography>
-//       <Typography variant="body1">
-//         Engage with our Arena Research Program where students and professionals collaborate on groundbreaking tech projects.
-//       </Typography>
-//     </Box>
-//     </Container>
-//   );
-// };
-
-// export default ArenaResearchProgram;
 
 'use client';
 import React from 'react';
