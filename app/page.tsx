ffaa87f6d7df2737cc0c4bae86f7c15f32acf27f
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-11-27 13:02:39 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,27 @@ import Hero_section from "@/components/Hero_section";
 import ProBattle_Event_section from "@/components/ProBattle_Event_section";
 import Social_section from "@/components/Social_section";
 
+import { Box } from "@mui/material";
+
 export default function Home() {
   return (
     <>
+      {/* Background Image */}
+      <Box
+        sx={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          backgroundImage: 'url(/gradient_bg_hero.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          zIndex: -2,
+        }}
+      />
+      
       <Hero_section />
       <CodeX_Event_section />
       {/* <Social_section /> */}
