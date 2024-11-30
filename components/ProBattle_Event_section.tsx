43db10a9f1ae91cd530bfe7932ad9f53fe624130
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-11-30 20:56:00 +0500

    Update ProBattle_Event_section.tsx

--- components/ProBattle_Event_section.tsx
+++ components/ProBattle_Event_section.tsx
@@ -121,14 +121,10 @@ const ProBattleSection: React.FC = () => {
               color="primary"
               endIcon={<PlayCircleOutlineIcon />} // Icon on the right side
               sx={{
-              backgroundColor: "#FFD700", // Gold color
-              color: "black",
-              fontWeight: "bold",
-              px: 4, // Extra padding for better button appearance
-              py: 1.5,
-              "&:hover": {
-                backgroundColor: "#FFA500", // Darker gold on hover
-              },
+                color: "black",
+                fontWeight: "semibold",
+                px: 4, // Extra padding for better button appearance
+                py: 1.5,
               }}
             >
               Get Started
