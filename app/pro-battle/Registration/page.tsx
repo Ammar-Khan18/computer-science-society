3bdee96942e47ee4919ece2c42089aa4a648fcb7
Ammar Khan <ammar.khaan05@gmail.com>
2025-01-08 00:48:11 +0500

    pro battle registration page and package updates

---
+++ app/pro-battle/Registration/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProBattleRegistration from '@/components/ProBattle_Registration';
+
+const RegisterPage: React.FC = () => {
+  return <ProBattleRegistration />;
+};
+
+export default RegisterPage;
