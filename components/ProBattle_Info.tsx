200ab348ba658073fd0cc1ae443d59a3e32f617c
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-01-10 14:15:41 +0500

    Update ProBattle_Info.tsx

--- components/ProBattle_Info.tsx
+++ components/ProBattle_Info.tsx
@@ -32,7 +32,7 @@ const EventCard: React.FC<{ event: Event }> = ({ event }) => (
         flexDirection: "column",
         height: "100%",
         borderRadius: 2,
-        boxShadow: "4px 6px #1B1A55",
+        boxShadow: "4px 6px #8D0B41",
         transition: "0.3s",
         overflow: "visible",
       }}
