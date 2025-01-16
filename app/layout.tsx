56c0f0bd6d50c7aa2ae06f6da9f92f4be6fb5f32
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-01-16 15:39:44 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         {/* <Navbar /> */}
         {/* {children} */}
         <Page />
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
