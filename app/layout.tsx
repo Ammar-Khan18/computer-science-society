cbc2c00ff00094f7d1326bf17d95f6188535cfbc
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-01-16 11:59:33 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,31 @@
-// "use server";
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import createTheme from "@mui/material/styles/createTheme";
-import { ThemeProvider } from "@mui/material";
+
+// Making temporary the pro battle page the main page
+import Page from "@/app/pro-battle/page";
 
 export const metadata: Metadata = {
   title: "IBA Computer Science Society",
   description: "Computer Science Society",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
 
-  // const theme = createTheme({
-  //   palette: {
-  //     mode: 'light',
-  //   },
-  // });
   return (
     <html lang="en">
-      {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" async /> */}
+      <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />
       <body className="page">
-        {/* <ThemeProvider theme={theme}> */}
-          <Navbar />
-          {children}
-          <Footer />
-        {/* </ThemeProvider> */}
+        <Navbar />
+        {/* {children} */}
+        <Page />
+        <Footer />
       </body>
-    </html >
+    </html>
   );
 }
