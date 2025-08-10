fbd97eaffec2ac9e8a67320ff8e45b5d49de872d
Ammar Khan <ammar.khaan05@gmail.com>
2025-08-10 21:25:49 +0500

    Removed: Every File

--- app/pro-battle/onboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ProBattleRegistration from '@/components/probattle/registration/form';
-
-const RegisterPage: React.FC = () => {
-  return <ProBattleRegistration />;
-};
-
-export default RegisterPage;
--- components/probattle/registration/types.tsx
+++
@@ -1,5 +0,0 @@
-export interface Module {
-  [x: string]: any;
-  id: any;
-    name: string, type: "attendee" | "regular", price: number, minMembers: number, maxMembers: number, start: string, end: string, tier: string, tierDescription: string
-}
