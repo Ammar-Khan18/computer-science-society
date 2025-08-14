c341e941080ff7ba79dc5073b584dbb96a61fe93
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-08-14 13:19:29 +0500

    Update page.tsx

--- app/Forms/page.tsx
+++ app/Forms/page.tsx
@@ -8,7 +8,7 @@ const FormsPage: React.FC = () => {
     <div className="relative min-h-screen font-title colour-bg pt-24 pb-8">
       <div className="flex items-center justify-center mb-8">
         <div className="flex-grow h-px bg-white" />
-        <span className="px-4 font-heading text-3xl md:text-4xl font-bold colour-text">
+        <span className="px-4 font-heading text-3xl md:text-4xl colour-text">
           Available Forms
         </span>
         <div className="flex-grow h-px bg-white" />
