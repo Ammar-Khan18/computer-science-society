c264cf4498c748d17b4ab4ad4dff1ae8676cc43b
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-08-14 13:18:45 +0500

    Update FormItem.tsx

--- components/Forms/FormItem.tsx
+++ components/Forms/FormItem.tsx
@@ -16,12 +16,12 @@ export const FormItem: React.FC<FormItemProps> = ({ form }) => (
   <div className="flex flex-col md:flex-row justify-between items-start md:items-center py-4 px-3 md:px-4 rounded-lg colour-box-primary shadow mb-4">
     <div className="flex flex-col gap-2">
       <div className="flex items-center gap-2">
-        <span className="font-heading text-lg font-bold colour-text">{form.title}</span>
-        <Badge variant={statusColor[form.status]}>{form.status}</Badge>
+        <span className="font-heading text-lg colour-text">{form.title}</span>
+        <Badge className="font-text" variant={statusColor[form.status]}>{form.status}</Badge>
       </div>
-      <span className="font-text text-white text-base">{form.description}</span>
+      <span className="font-text text-[10px] md:text-[12px] text-white text-base">{form.description}</span>
     </div>
-    <Button asChild variant="default" className="mt-4 md:mt-0">
+    <Button asChild variant="default" className="mt-4 ml-0 md:ml-1 md:mt-0 font-heading">
       <a href={form.link} target="_blank" rel="noopener noreferrer">Open Form</a>
     </Button>
   </div>
