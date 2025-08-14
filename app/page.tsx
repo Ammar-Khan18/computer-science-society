f706323c2ae13545bf074e9708dc368222b474de
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-08-14 21:42:40 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { HomeCarousel } from "@/components/Home/Carousel";
 const Home: React.FC = () => {
   return (
     <div className="colour-bg min-h-screen">
-      <h1 className="text-center font-title text-8xl p-4 colour-accent mb-8">IBA <br /> Computer Science Society</h1>
+      <h1 className="text-center font-title text-4xl md:text-8xl p-4 colour-accent mb-8">IBA <br /> Computer Science Society</h1>
       <HomeCarousel />
     </div>
   );
