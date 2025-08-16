4f24b42f9d2ee3e033f3478caa718bdc4c2ba823
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-08-16 13:42:12 +0500

    Update welcomeAnimation.tsx

--- components/Home/welcomeAnimation.tsx
+++ components/Home/welcomeAnimation.tsx
@@ -4,7 +4,7 @@ import Welcome from '../../public/Home/Welcome.json';
 
 const WelcomeAnimation: React.FC = () => {
     return (
-        <div className='bg-[#0a192f] flex justify-center items-center bg-transparent'> 
+        <div className='flex justify-center items-center bg-transparent'> 
             <DisplayLottie animationData={Welcome} />
         </div>
     );
