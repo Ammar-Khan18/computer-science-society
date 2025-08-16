e97dccbb8178444603db9db9ad2e7e5c93b90619
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-08-16 16:26:09 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import TitleSection from "@/components/Home/TitleSection";
 import DescriptionSection from "@/components/Home/DescriptionSection";
 import CodeX from "@/components/Home/CodeX";
 import ProBattle from "@/components/Home/ProBattle";
-import DevelopersSection from "@/components/Home/DevelopersSection";
+// import DevelopersSection from "@/components/Home/DevelopersSection";
+import TeamSection from "@/components/Home/TeamSection";
 
 const Home: React.FC = () => {
   return (
@@ -15,7 +16,8 @@ const Home: React.FC = () => {
       <DescriptionSection />
       <CodeX />
       <ProBattle />
-      <DevelopersSection />
+      {/* <DevelopersSection /> */}
+      <TeamSection />
     </div>
   );
 };
