25e726c2e200c256e4d5749f371453e5a25229e8
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-08-17 19:19:41 +0500

    Update constants.ts

--- app/Forms/constants.ts
+++ app/Forms/constants.ts
@@ -9,27 +9,9 @@ export interface Form {
 
 export const forms: Form[] = [
   {
-    title: "Form 1",
-    description: "This is the first form",
-    link: "https://www.google.com",
+    title: "Orientation Registration Form",
+    description: "This form is for the management team positions, circulated during O `Gala 2025.",
+    link: "https://forms.gle/WoPPxGfPCcMmyVMAA",
     status: "Open",
-  },
-  {
-    title: "Form 2",
-    description: "This is the second form",
-    link: "https://www.google.com",
-    status: "Open",
-  },
-  {
-    title: "Form 3",
-    description: "This is the third form",
-    link: "https://www.google.com",
-    status: "Closed",
-  },
-  {
-    title: "Form 4",
-    description: "This is the fourth form with a long description, much more will be added when this will be done",
-    link: "https://www.google.com",
-    status: "Closed",
   },
 ];
