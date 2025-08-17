a6225a295ed829ddff4471d453c1de9ca3204789
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-08-17 19:15:09 +0500

    Update page.tsx

--- app/ProBattle/page.tsx
+++ app/ProBattle/page.tsx
@@ -1,14 +1,7 @@
 export default function ProBattle() {
   return (
-    <div className="font-title colour-bg">
-      <h1 className="font-heading colour-primary text-6xl">Welcome <br /> to IBA <br /> Computer Science Society</h1>
-      <p className="font-text colour-secondary text-4xl">
-        A community for computer science enthusiasts at IBA.
-        A community for computer science enthusiasts at IBA.
-        A community for computer science enthusiasts at IBA.
-        A community for computer science enthusiasts at IBA.
-        A community for computer science enthusiasts at IBA.
-      </p>
+    <div className="min-h-screen colour-bg flex items-center justify-center">
+      <h1 className="font-title colour-text text-6xl md:text-9xl text-center">Coming Soon</h1>
     </div>
   );
 }
