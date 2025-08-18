7fdd4fe5d40a32ef0f12eefa22cf2ab8f1f89fcc
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-08-18 14:00:44 +0500

    Update CodexJoin.tsx

--- components/Codex/CodexJoin.tsx
+++ components/Codex/CodexJoin.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { MessageCircle } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 const whatsappLink = "https://chat.whatsapp.com/LM8BwRVyqha33PXq1JJfmU?mode=ac_t";
 
 const CodexJoin: React.FC = () => (
-  <section className="flex flex-col items-center my-30 py-10 px-4 md:px-12">
+  <section className="flex flex-col items-center my-30 py-10 px-4 md:px-12 gap-4">
     <h2 className="font-heading text-2xl md:text-3xl colour-text mb-6 text-center tracking-tight">Ready to Join the Community?</h2>
     <Button
       asChild
@@ -18,6 +19,20 @@ const CodexJoin: React.FC = () => (
         <span className="font-text">Join WhatsApp Community</span>
       </Link>
     </Button>
+
+    <div className="flex items-center justify-center">
+        <div className="flex-grow h-px bg-white" />
+        <span className="px-4 font-text text-sm md:text-md colour-text">
+          Or
+        </span>
+        <div className="flex-grow h-px bg-white" />
+    </div>
+
+    <div className="flex flex-col items-center">
+      <Image src="/Codex/WhatsApp.png" alt="WhatsApp" width={250} height={250} className="object-contain pb-2" />
+      <span className="font-text text-sm md:text-md colour-text">Scan QR code</span>
+    </div>
+
   </section>
 );
 
