58e15541b69269dce185f5b93fb3e2cc6c73c1b0
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-08-24 15:59:53 +0500

    Update FormList.tsx

--- components/Forms/FormList.tsx
+++ components/Forms/FormList.tsx
@@ -8,8 +8,16 @@ interface FormListProps {
 
 export const FormList: React.FC<FormListProps> = ({ forms }) => (
   <div className="w-full max-w-2xl mx-auto px-2">
-    {forms.map((form, idx) => (
-      <FormItem key={form.title} form={form} />
-    ))}
+    {forms.length === 0 ? (
+      <div className="flex h-[200px] w-full items-center justify-center">
+        <div className="flex h-[200px] w-[350px] items-center justify-center rounded-md border border-dashed text-sm">
+          <p className="font-text colour-text">No forms</p>
+        </div>
+      </div>
+    ) : (
+      forms.map((form, idx) => (
+        <FormItem key={form.title} form={form} />
+      ))
+    )}
   </div>
 );
