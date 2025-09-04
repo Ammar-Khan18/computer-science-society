cc07e6f3585e41f06be6c612832a08a516267d88
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-09-05 00:34:50 +0500

    Update page.tsx

--- app/ProBattle/page.tsx
+++ app/ProBattle/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import * as React from "react";
+import Link from "next/link";
 
 const ProBattle: React.FC = () => {
   return (
-    <main className="grid min-h-screen place-items-center colour-bg px-6 py-24 sm:py-32 lg:px-8">
+    <main className="min-h-screen items-center colour-bg px-6 pt-40">
       <div className="text-center">
         <p className="text-lg font-semibold text-indigo-600">404</p>
         <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance colour-accent sm:text-7xl">
@@ -14,12 +15,12 @@ const ProBattle: React.FC = () => {
           Sorry, this page is not available yet.
         </p>
         <div className="mt-10 flex items-center justify-center">
-          <a
+          <Link
             href="/"
             className="rounded-md bg-secondary px-3.5 py-2.5 text-sm font-semibold text-secondary-foreground shadow-xs hover:bg-secondary/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
           >
             Go back home
-          </a>
+          </Link>
         </div>
       </div>
     </main>
