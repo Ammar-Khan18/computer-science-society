52d08d5894f29db581addba251089b7749020c3e
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-09-05 00:37:57 +0500

    Update page.tsx

--- app/ProBattle/page.tsx
+++ app/ProBattle/page.tsx
@@ -8,16 +8,16 @@ const ProBattle: React.FC = () => {
     <main className="min-h-screen items-center colour-bg px-6 pt-40">
       <div className="text-center">
         <p className="text-lg font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance colour-accent sm:text-7xl">
+        <h1 className="mt-4 text-6xl font-title tracking-tight text-balance colour-accent md:text-8xl">
           Coming Soon!
         </h1>
-        <p className="mt-6 text-lg font-medium text-pretty colour-text sm:text-xl/8">
+        <p className="mt-6 text-md font-text font-medium text-pretty colour-text md:text-xl/8">
           Sorry, this page is not available yet.
         </p>
         <div className="mt-10 flex items-center justify-center">
           <Link
             href="/"
-            className="rounded-md bg-secondary px-3.5 py-2.5 text-sm font-semibold text-secondary-foreground shadow-xs hover:bg-secondary/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
+            className="rounded-md bg-secondary px-3.5 py-2.5 text-sm font-text font-semibold text-secondary-foreground shadow-xs hover:bg-secondary/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
           >
             Go back home
           </Link>
