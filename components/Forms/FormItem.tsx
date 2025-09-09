505949257b220ef5aeaf4da0a23d4adf6b80a586
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-09-09 20:40:20 +0500

    Update FormItem.tsx

--- components/Forms/FormItem.tsx
+++ components/Forms/FormItem.tsx
@@ -19,9 +19,9 @@ export const FormItem: React.FC<FormItemProps> = ({ form }) => (
     <div className="flex flex-col gap-2">
       <div className="flex items-center gap-2">
         <span className="font-heading text-lg colour-text">{form.title}</span>
-        <Badge className="font-text" variant={statusColor[form.status]}>{form.status}</Badge>
+        <Badge className="font-text text-sm" variant={statusColor[form.status]}>{form.status}</Badge>
       </div>
-      <span className="font-text text-[10px] md:text-[12px] text-white text-base">{form.description}</span>
+      <span className="font-text text-[14px] md:text-[15px] text-white text-base">{form.description}</span>
     </div>
     {form.status === "Closed" || form.status === "Coming Soon" ? (
       <Button variant="default" className="mt-4 ml-0 md:ml-1 md:mt-0 font-text colour-text" disabled>
