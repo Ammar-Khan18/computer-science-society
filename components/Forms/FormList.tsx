f5cd17867f9ded9d5c63cb9981bbc89d4d5e1c5d
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-09-10 21:02:34 +0500

    Update FormList.tsx

--- components/Forms/FormList.tsx
+++ components/Forms/FormList.tsx
@@ -8,6 +8,10 @@ interface FormListProps {
 
 export const FormList: React.FC<FormListProps> = ({ forms }) => (
   <div className="w-full max-w-2xl mx-auto px-2">
+    
+    {/* Background pattern */}
+    <div className="z-0 absolute inset-0 bg-[radial-gradient(circle,rgba(255,255,255,0.2)_1px,transparent_1px)] bg-[length:40px_40px]" />
+       
     {forms.length === 0 ? (
       <div className="flex h-[200px] w-full items-center justify-center">
         <div className="flex h-[200px] w-[350px] items-center justify-center rounded-md border border-dashed text-sm">
@@ -24,4 +28,3 @@ export const FormList: React.FC<FormListProps> = ({ forms }) => (
     )}
   </div>
 );
-
