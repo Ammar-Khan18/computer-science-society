52ae5d5a50b38d64981307fe604673baa452d13f
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-09-14 23:49:20 +0500

    Update TeamSection.tsx

--- components/Home/TeamSection.tsx
+++ components/Home/TeamSection.tsx
@@ -4,6 +4,17 @@ import { FaLinkedin, FaInstagram } from "react-icons/fa";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
+// variants for text: from top (hidden) to its position (visible)
+const textVariants = {
+  hidden: { opacity: 0, y: -50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.8, ease: "easeOut" as const },
+  },
+};
 
 const members = [
   {
@@ -32,8 +43,26 @@ const members = [
 const TeamSection: React.FC = () => (
   <section className="py-12 px-4 md:px-12 mt-30 min-h-[600px] colour-bg mb-20">
     <div className="justify-center items-center mb-30">
-      <h2 className="font-heading text-4xl md:text-5xl colour-text mb-6 text-center">Meet our team</h2>
-      <p className="text-center font-text md:text-lg text-[#bfc8e6] max-w-3xl mb-10 mx-auto">We&apos;re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our members.</p>
+
+      <motion.h2 
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 1.0 }}
+        variants={textVariants}
+        className="font-heading text-4xl md:text-5xl colour-text mb-6 text-center"
+      >
+          Meet our team
+      </motion.h2>
+
+      <motion.p
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 1.0 }}
+        variants={textVariants}
+        className="text-center font-text md:text-lg text-[#bfc8e6] max-w-3xl mb-10 mx-auto"
+      >
+        We&apos;re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our members.
+      </motion.p>
     </div>
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-45 place-items-center max-w-4xl mx-auto">
       {members.map((dev) => (
